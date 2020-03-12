117f159e47dffc4ee37dfcf84d1f3ddeb1253309
mahmodcodeforiraq <62080124+mahmodcodeforiraq@users.noreply.github.com>
2020-03-12 14:15:19 +0300

    Update AboutTeam.dart

--- lib/About/AboutTeam.dart
+++ lib/About/AboutTeam.dart
@@ -16,7 +16,7 @@ class StateAboutTeam extends State<AboutTeam> {
           shape: RoundedRectangleBorder(
               borderRadius:
                   new BorderRadius.only(bottomRight: Radius.circular(35))),
-          title: new Text("دليل اطباء صلاح الدين"),
+          title: new Text("عن الفريق"),
           centerTitle: true,
           backgroundColor: Color(0xff6A4BC3),
           actions: <Widget>[],
