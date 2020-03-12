e22ab003345e4af27f8aaf97381ecf4d07c5f7e6
mahmodcodeforiraq <62080124+mahmodcodeforiraq@users.noreply.github.com>
2020-03-12 14:14:32 +0300

    Update AboutApp.dart

--- lib/About/AboutApp.dart
+++ lib/About/AboutApp.dart
@@ -15,7 +15,7 @@ class StateAboutApp extends State<AboutApp> {
           shape: RoundedRectangleBorder(
               borderRadius:
                   new BorderRadius.only(bottomRight: Radius.circular(35))),
-          title: new Text("دليل اطباء صلاح الدين"),
+          title: new Text("عن التطبيق"),
           centerTitle: true,
           backgroundColor: Color(0xff6A4BC3),
           actions: <Widget>[],
